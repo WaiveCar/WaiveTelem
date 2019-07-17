5aed9f40998231a06d6e218b5dfec5de0f93d6ba
Alan Chen <alanpc@gmail.com>
2019-07-17 00:32:21 -0700

    update flashFirmware.sh for windows

--- packages/plug-provision/flashFirmware.sh
+++ packages/plug-provision/flashFirmware.sh
@@ -2,10 +2,19 @@
 
 echo "Running flashFirmware.sh"
 
-PORT=$(pio device list --json-output | jq '.[] | select(.description | contains("Arduino")) | .port ')
-PORT="${PORT%\"}"
-PORT="${PORT#\"}"
+case "$(uname -s)" in
+   Darwin)
+      PORT=$(pio device list --json-output | jq '.[] | select(.description | contains("Arduino")) | .port ')
+      PORT="${PORT%\"}"
+      PORT="${PORT#\"}"
 
-stty -f ${PORT} 1200 \
-  && sleep 3 \
-  && ~/.platformio/packages/tool-bossac/bossac --info --debug --port "${PORT}" --erase --write --verify --reset -U true ${DEFAULT_FIRMWARE}
+      stty -f ${PORT} 1200 \
+        && sleep 3 \
+        && ~/.platformio/packages/tool-bossac/bossac --info --debug --port "${PORT}" --erase --write --verify --reset -U true ${DEFAULT_FIRMWARE}
+
+      ;;
+   *)
+      (cd ../plug-firmware; pio run -e $BOARD -t build)
+      ;;
+esac
+
--- packages/plug-provision/provision.sh
+++ packages/plug-provision/provision.sh
@@ -3,7 +3,10 @@
 PLUG_ID=plug-1
 TEMPLATE=./config/hyukia.txt
 DEFAULT_FIRMWARE=../plug-firmware/bin/1000_1.0.4_9ccd5d8eb348015145a4df52cd29e7e233768a9d2fc475e4aab4773fc5cccb66
-SD_VOLUME=/Volumes/SDCARD
+#SD_VOLUME=/Volumes/SDCARD
+SD_VOLUME=/d
+#BOARD=mkrnb1000
+BOARD=mkrnb1500
 
 #. generateCSR.sh
 #. registerAWS.sh
