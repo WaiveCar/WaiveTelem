c647557fb544b739dd4e9ba51703dee19aecf587
Alan Chen <alan.chen@ravelab.com>
2019-08-30 12:41:37 -0700

    uncomment Mqtt.poll()

---
+++ packages/plug-firmware/include/Motion.h
@@ -0,0 +1,12 @@
+#ifndef Motion_h
+#define Motion_h
+
+class MotionClass {
+ public:
+  int begin();
+  void poll();
+};
+
+extern MotionClass Motion;
+
+#endif  // Motion_h
--- packages/plug-firmware/src/Main.cpp
+++ packages/plug-firmware/src/Main.cpp
@@ -5,8 +5,6 @@
 #ifndef ARDUINO_SAMD_MKR1000
 #include <Modem.h>
 #endif
-// #include <MPU6050_tockn.h>
-// #include <Wire.h>
 
 #include "Bluetooth.h"
 #include "Can.h"
@@ -15,11 +13,10 @@
 #include "Gps.h"
 #include "Internet.h"
 #include "Logger.h"
+#include "Motion.h"
 #include "Mqtt.h"
 #include "Pins.h"
 #include "System.h"
-
-// MPU6050 mpu6050(Wire);
 
 void setup() {
   Serial.begin(115200);
@@ -30,11 +27,10 @@ void setup() {
 
   Pins.begin();
   int eccInit = ECCX08.begin();
-  // mpu6050.begin();
-  // mpu6050.calcGyroOffsets(true);
   int sdInit = SD.begin(SD_CS_PIN);
-  int cfgInit = Config.begin();
   int loggerInit = Logger.begin();
+  Motion.begin();
+  int cfgInit = Config.begin();
   int cmdInit = Command.begin();
   System.begin();
   int mqttInit = Mqtt.begin();
@@ -62,8 +58,6 @@ void setup() {
   System.sendInfo(sysJson);
 }
 
-long timer = 0;
-
 void loop() {
   Watchdog.reset();
   if (!System.stayResponsive()) {
@@ -71,50 +65,8 @@ void loop() {
   }
   System.keepTime();
 
-  // Mqtt.poll();
+  Mqtt.poll();
   Bluetooth.poll();
   Can.poll();
   System.poll();
-
-  // mpu6050.update();
-
-  // if (millis() - timer > 1000) {
-  //   Serial.println("=======================================================");
-  //   Serial.print("temp : ");
-  //   Serial.println(mpu6050.getTemp());
-  //   Serial.print("accX : ");
-  //   Serial.print(mpu6050.getAccX());
-  //   Serial.print("\taccY : ");
-  //   Serial.print(mpu6050.getAccY());
-  //   Serial.print("\taccZ : ");
-  //   Serial.println(mpu6050.getAccZ());
-
-  //   Serial.print("gyroX : ");
-  //   Serial.print(mpu6050.getGyroX());
-  //   Serial.print("\tgyroY : ");
-  //   Serial.print(mpu6050.getGyroY());
-  //   Serial.print("\tgyroZ : ");
-  //   Serial.println(mpu6050.getGyroZ());
-
-  //   Serial.print("accAngleX : ");
-  //   Serial.print(mpu6050.getAccAngleX());
-  //   Serial.print("\taccAngleY : ");
-  //   Serial.println(mpu6050.getAccAngleY());
-
-  //   Serial.print("gyroAngleX : ");
-  //   Serial.print(mpu6050.getGyroAngleX());
-  //   Serial.print("\tgyroAngleY : ");
-  //   Serial.print(mpu6050.getGyroAngleY());
-  //   Serial.print("\tgyroAngleZ : ");
-  //   Serial.println(mpu6050.getGyroAngleZ());
-
-  //   Serial.print("angleX : ");
-  //   Serial.print(mpu6050.getAngleX());
-  //   Serial.print("\tangleY : ");
-  //   Serial.print(mpu6050.getAngleY());
-  //   Serial.print("\tangleZ : ");
-  //   Serial.println(mpu6050.getAngleZ());
-  //   Serial.println("=======================================================\n");
-  //   timer = millis();
-  // }
 }
