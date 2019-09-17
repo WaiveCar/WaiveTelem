9d6a26b9ea0d9e7899eec7754bca24df5c9ca230
Alan Chen <alan.chen@ravelab.com>
2019-09-17 16:01:50 -0700

    check in .pio/libdeps so not relying on cloud

--- packages/plug-firmware/setEnv.sh
+++ packages/plug-firmware/setEnv.sh
@@ -7,9 +7,7 @@ ln -s -f $PWD/bsp/waive1000 ~/.platformio/packages/framework-arduinosam/variants
 sed -i'.bak' -e 's/adapter_khz\ 400/adapter_khz\ 5000/g' ~/.platformio/packages/tool-openocd/scripts/target/at91samdXX.cfg
 sed -i'.bak' -e 's/static\ volatile\ uint32_t\ _ulTickCount/volatile\ uint32_t\ _ulTickCount/g' ~/.platformio/packages/framework-arduinosam/cores/samd/delay.c
 
-pio lib update
-
-export FIRMWARE_VERSION=1.1.8
+export FIRMWARE_VERSION=1.1.9
 
 export S3_HOST=waiveplug.s3.us-east-2.amazonaws.com
 export DEBUG=
